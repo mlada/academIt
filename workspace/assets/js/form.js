74548168a1e8f108de76c7cc1fa3c90f6d12ccb8
241 <241@smsfinance.ru>
2017-07-19 15:29:44 +0700

    table

--- workspace/assets/js/form.js
+++ workspace/assets/js/form.js
@@ -1,12 +1,24 @@
-function checkInput(e){
- 	var theTarget = e.target
-	if (theTarget.value.length < 4){
-		theTarget.className = " error";
+(function() {
+	'use strict';
+
+	document.addEventListener("DOMContentLoaded", function(event) {
+		init();
+	});
+
+	function init(){
+		function checkInput(e){
+			var theTarget = e.target
+			if (theTarget.value.length < 4){
+				theTarget.className = " error";
+			}
+			else{
+				theTarget.className = " ";
+			}
+		}
+		document.getElementById('name').addEventListener('keyup',checkInput,false);
+		document.getElementById('surname').addEventListener('keyup',checkInput,false);
+		document.getElementById('about').addEventListener('keyup',checkInput,false);
 	}
-	else{
-		theTarget.className = " ";
-	}
-}
-document.getElementById('name').addEventListener('keyup',checkInput,false);
-document.getElementById('surname').addEventListener('keyup',checkInput,false);
-document.getElementById('about').addEventListener('keyup',checkInput,false);
+
+}());
+
