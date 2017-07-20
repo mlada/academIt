7cf2172e019dee122aa2946b83a83df672a705f8
241 <241@smsfinance.ru>
2017-07-20 12:26:30 +0700

    hw-4

--- workspace/assets/js/table.js
+++ workspace/assets/js/table.js
@@ -1,13 +1,26 @@
-(function() {
+(function () {
     'use strict';
 
-    document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("DOMContentLoaded", function (event) {
         init();
     });
 
-    function init(){
-        var tab = document.getElementById('mendeleev');
-        
+    function init() {
+        var rows = document.getElementById('mendeleev').childNodes[1].children;
+        (function () {
+            for (var i = 0; i < rows.length; i++) {
+                (i + 1 & 1) ? ( rows[i].className = "") : (
+                    (function () {
+                        for (var j = 0; j < rows[i].children.length; j++) {
+                            (j + 1 & 1) ? ( rows[i].children[j].className = "") :
+                                rows[i].children[j].onmouseover = function (event) {
+                                    var target = event.target;
+                                    target.classList.add('bg');
+                                }
+                        }
+                    })()
+                );
+            }
+        })();
     }
-
 }());
