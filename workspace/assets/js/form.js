397e4cc10defbc2b860af705dd48f3816bcca5e3
241 <241@smsfinance.ru>
2017-07-21 14:14:36 +0700

    create user

--- workspace/assets/js/form.js
+++ workspace/assets/js/form.js
@@ -18,7 +18,22 @@
 		document.getElementById('name').addEventListener('keyup',checkInput,false);
 		document.getElementById('surname').addEventListener('keyup',checkInput,false);
 		document.getElementById('about').addEventListener('keyup',checkInput,false);
+
+		function User() {
+			this.name = document.getElementById('name').value;
+			this.surname = document.getElementById('surname').value;
+			this.country = document.querySelectorAll("option[name='country']:checked").value;
+			this.subscribe = document.querySelectorAll("input[name='subsctibe']").value;
+			this.education = document.querySelectorAll("input[name='education']:checked").value;
+			this.about = document.getElementById('about').value;
+		}
+
+		function checkForm(id){
+			
+		}
+		document.getElementById('submit').addEventListener('click',checkForm('user'),false);
 	}
+	
 
 }());
 
