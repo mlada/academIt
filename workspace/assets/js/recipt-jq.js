5165ae85f256c981c72ba18ecfb61d046449f853
241 <241@smsfinance.ru>
2017-07-25 14:18:02 +0700

    recipts js -> jquery

--- workspace/assets/js/recipt-jq.js
+++ workspace/assets/js/recipt-jq.js
@@ -1,8 +1,9 @@
-$(function(){
+$(window).on("load",function(){
     var products = ["400 мл молока","550 г муки","20-25 г сырых дрожжей","20 г сливочного масла","2 ст.л. растительного масла","0,5 ст.л. соли ","1,5 ст.л. сахара","500 г адыгейского сыра","яйца"],
         includeBlock=$("#ingrdients"),
         ul=$("<ul></ul>"),
         li=$("<li></li>");
+    console.log(includeBlock);
     products.each(function(i){
         li.text(i).appendTo(ul);
     }).appendTo(includeBlock);
