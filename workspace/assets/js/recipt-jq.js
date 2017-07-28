d47e3dddc03d33596918f988c086838ccca97b33
241 <241@smsfinance.ru>
2017-07-28 10:54:38 +0700

    hw 5 рецепты  и табл

--- workspace/assets/js/recipt-jq.js
+++ workspace/assets/js/recipt-jq.js
@@ -1,27 +1,30 @@
 $(window).on("load",function(){
+
     var products = ["400 мл молока","550 г муки","20-25 г сырых дрожжей","20 г сливочного масла","2 ст.л. растительного масла","0,5 ст.л. соли ","1,5 ст.л. сахара","500 г адыгейского сыра","яйца"],
         includeBlock=$("#ingrdients"),
-        ul=$("<ul></ul>"),
-        li=$("<li></li>");
-    $("#recipts-img").on("click",function(e){e.target.remove()});
-    includeBlock.append(ul.append($.each(products,function(i){
-            li.text(products[i]);
-        })
-    ));
+        div ='';
+
+
+    $.each(products,function(i){
+        div += '<div>'+products[i]+'</div>';
+    });
+    includeBlock.append(div);
+
+    console.log(div);
     var term = "хачапури";
     var locale_HTML = document.body.innerHTML;
     var termArr =["хачапури","самые","вкусные"];
-    var placeSearch = $('#accordion').html();
+    var placeSearch = $('#reciptBlock').html();
     /*var el = placeSearch.replace(eval("/"+term+"/g"),"#"+term);
     $('#accordion').html(el);*/
     var elArr;
     $(termArr).each(function (i) {
         var termEl = termArr[i];
-        elArr = placeSearch.replace(eval("/"+termEl+"/g"),"#"+termEl);
+        elArr = placeSearch.replace(eval("/"+termEl+"/g"),'<strong>'+termEl+'</strong>');
         placeSearch = elArr;
-        console.log(elArr);
+        // console.log(elArr);
     });
-    $('#accordion').html(elArr);
+    $('#reciptBlock').html(elArr);
 
 
 /*
@@ -30,6 +33,20 @@ $(window).on("load",function(){
     var el = placeSearch.replace(eval("/"+term+"/g"),"#"+term);
     $('body').html(el);
 */
+    $("#accordion")
+        .accordion({
+            header: "> div > h2"
+        })
+        .sortable({
+            axis: "y",
+            handle: "h2",
+            stop: function (event, ui) {
+                ui.item.children("h2").triggerHandler("focusout");
+                $(this).accordion("refresh");
+            }
+        });
+
+    $("#recipts-img").on("click",function(e){e.target.remove()});
 
 
 });
--- workspace/assets/js/table-jq.js
+++ workspace/assets/js/table-jq.js
@@ -5,16 +5,32 @@ $(window).on("load", function () {
         }
     });
     var elem = [];
+    /*$('TD').bind("click", function (event) {
+     if (elem.length < 2) {
+     elem.push(event.target);
+     console.log(elem);
+     }
+     else{
+     var sb1 = elem[1].previousElementSibling;
+     $(elem[0].previousElementSibling).after($(elem[1]));
+     $(sb1).after($(elem[0]).detach());
+     elem = [];
+     }
+     });*/
+
     $('TD').bind("click", function (event) {
-        if (elem.length < 2) {
             elem.push(event.target);
-            console.log(elem);
-        }
-        else{
-            var sb1 = elem[1].previousElementSibling;
-            $(elem[0].previousElementSibling).after($(elem[1]));
-            $(sb1).after($(elem[0]).detach());
-            elem = [];
-        }
+            while (elem.length == 2) {
+                var a = $(elem[0]).text();
+                $(elem[0]).text($(elem[1]).text());
+                $(elem[1]).text(a);
+                elem =[];
+                break;
+            }
     });
+
+   /* while (elem.length == 2) {
+        console.log($(elem[0]).text());
+        // console.log(elem[1].text());
+    }*/
 });
