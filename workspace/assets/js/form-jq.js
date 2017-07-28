ac08c2e748948ba9df3ee64efd73086f8b6f8bf7
241 <241@smsfinance.ru>
2017-07-28 11:27:48 +0700

    hw 5 форма

---
+++ workspace/assets/js/form-jq.js
@@ -0,0 +1,30 @@
+$(window).on("load", function () {
+
+    function checkInput(e) {
+        var theTarget = e.target;
+        if (theTarget.value.length < 4) {
+            $(theTarget).addClass("error");
+        }else {
+            $(theTarget).removeClass("error");
+        }
+    }
+    $('#name,#surname,#about').keyup(checkInput);
+
+    function createUser() {
+        this.name = $('#name').val() + ' ' + $('#surname').val() ;
+        this.about = $('#about').val();
+        this.subscribe = $("#subscribe").is(":checked");
+        this.education = $("[name='education']:checked").val();
+        this.country = $("[name='country']:checked").val();
+        console.log(this);
+    }
+
+    $('#submit').bind('click',
+        function (e) {
+            e.preventDefault();
+            var os = new createUser;
+        });
+});
+
+
+
