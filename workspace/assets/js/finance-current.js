b53064ab21517cbd94d9d7f03fa0f1d8bf64dde3
241 <241@smsfinance.ru>
2017-08-01 14:46:25 +0700

    hw 5 promise currency

---
+++ workspace/assets/js/finance-current.js
@@ -0,0 +1,39 @@
+$(window).on("load", function () {
+    function showLoading() {
+        $("#black-wrap").show();
+        $("#loader").show();
+    }
+
+    function stopLoading() {
+        $("#black-wrap").hide();
+        $("#loader").hide();
+    }
+
+    var url = 'http://api.fixer.io/latest';
+    showLoading();
+    var noAfricaArr = [];
+    var newArr = [];
+    var noAsiaArr = [];
+    $.when($.get(url))
+        .then(function (currency) {
+            var arr = Lazy(currency)
+                .source.rates;
+            return arr;
+        })
+        .then(function(arr){
+            var cur = [arr.USD,arr.RUB];
+            return cur;
+        })
+        .then(function(cur){
+            $('body').append('<div class="currency"><div class="euro"><p>Курс евро</p><span>'+cur[1].toFixed(2)+'</span></div>' +
+                '<div class="dollar"><p>Курс доллара</p><span>'+(cur[1]/cur[0]).toFixed(2)+'</span></div></div> ' )
+            console.log (cur);
+        })
+        .fail(function (xhr, textStatus, errorThrow) {
+            console.log('error');
+        })
+        .always(function (xhr, textStatus) {
+            stopLoading();
+        });
+
+});
--- workspace/assets/js/finance.js
+++ workspace/assets/js/finance.js
@@ -9,86 +9,32 @@ $(window).on("load", function () {
         $("#loader").hide();
     }
 
-    /*/!*ajax*!/
-     var xhr;
-     if(window.ActiveXObject){
-     xhr = new ActiveXObject("Microsoft.XMLHTTP");
-     }else if (window.XMLHttpRequest){
-     xhr = new XMLHttpRequest();
-     }else{
-     throw new Error("Ajax not supported");
-     }*/
-
-    /*  /!*promise when/then*!/
-     var url = 'https://restcountries.eu/rest/v2/lang/ru';
-     showLoading();
-     $.when($.get(url))
-     .then(function(countries){
-     var countLength = Lazy(countries).length();
-     return countLength;
-     }).then(function (countLength){
-     console.log(countLength);
-     })
-     .fail(function (xhr,textStatus,errerThrow) {
-
-     })
-     .always(function (xhr,textStatus) {
-     stopLoading();
-     });*/
-
-    /*создание promice*/
-    /*    var url = 'https://restcountries.eu/rest/v2/lang/ru';
-     function asyncEvent() {
-     var dfd = jQuery.Deferred(); // создание промиса
-     setTimeout(function () {
-     dfd.resolve('hurray');
-     },2);
-     setTimeout(function () {
-     dfd.reject('sorry');
-     },2);
-     setTimeout(function working() {
-     if (dfd.state() === "pending"){
-     dfd.notify('working...');
-     setTimeout(working,500);
-     }
-     },1);
-     return dfd.promise();
-     }
-
-     $.when(asyncEvent()).then(
-     function (status) {
-     console.log (status + ", все супер! ");
-     },
-     function (status) {
-     console.log (status + ", что-то пошло не так! ");
-     },
-     function (status) {
-     $("body").append(status);
-     }
-     );*/
-
-    /*Пример с jquery*/
-    /*  $.get(url)
-     .done(function (countries,textStatus,xhr) {
-     var countLength = Lazy(countries).length();
-     console.log(countLength);
-     })
-     .fail(function (xhr,textStatus,errorThrown){}
-     )
-     .always(function (xhr,textStatus){}
-     );*/
-
-
-    var url = 'https://restcountries.eu/rest/v2/lang/ru';
+    var url = 'https://restcountries.eu/rest/v2/';
     showLoading();
     var noAfricaArr = [];
+    var newArr = [];
     var noAsiaArr = [];
     $.when($.get(url))
         .then(function (countries) {
             Lazy(countries)
                 .source
+                .forEach(function (con) {
+                    /*if (e.currencies["code"] !== "USD"){
+                     newArr.push(e);
+                     }*/
+                    con.currencies.forEach(function (cur) {
+                        if (cur.code === "USD") {
+                            newArr.push(con);
+                        }
+                    });
+
+                });
+            return newArr;
+        })
+        .then(function (newArr) {
+            Lazy(newArr)
                 .forEach(function (e) {
-                    if (e.region !== 'africa'){
+                    if (e.region !== 'africa') {
                         noAfricaArr.push(e);
                     }
                 });
@@ -97,7 +43,7 @@ $(window).on("load", function () {
         .then(function (noAfricaArr) {
             Lazy(noAfricaArr)
                 .forEach(function (e) {
-                    if (e.region !== 'Asia'){
+                    if (e.region !== 'Asia') {
                         noAsiaArr.push(e);
                     }
                 });
@@ -109,7 +55,7 @@ $(window).on("load", function () {
             console.log(noAsiaLength);
         })
         .fail(function (xhr, textStatus, errorThrow) {
-           console.log('error');
+            console.log('error');
         })
         .always(function (xhr, textStatus) {
             stopLoading();
