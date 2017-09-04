40ba738c57def1ae03756b7a1d478d5e48974398
mlada <mlada.pechorina@gmail.com>
2017-09-04 15:16:49 +0700

    states

--- react-redux-ts/src/models/state/globalState.tsx
+++ react-redux-ts/src/models/state/globalState.tsx
@@ -1,9 +1,9 @@
 import {IAppState} from './appState';
 import {IUserState} from './pages/userState';
-import {IPassportState} from './pages/passportState';
+// import {IPassportState} from './pages/passportState';
 
 export interface IGlobalState{
-    app: IAppState;
     user: IUserState;
-    passport: IPassportState;
+    app: IAppState;
+// passport: IPassportState;
 }
